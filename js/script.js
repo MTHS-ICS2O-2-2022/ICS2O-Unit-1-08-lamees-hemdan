3ebbae9355c283c9a624c4b73d581df5f9064e76
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-17 17:27:49 +0000

    deploy pages

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@
 // Created on: March 2023
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-lamees-hemdan/sw.js", {
+    scope: "/ICS2O-Unit-1-08-lamees-hemdan/",
+  })
+}
+
+
   function myButtonClicked() {
     alert("Hello,World!")
   }
